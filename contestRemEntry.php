663e855cc8a1d4660cdae94a8cfcaa650d776e92
Rick Smoke <rsmoke@umich.edu>
2016-06-09 11:54:50 -0400

    Fix update timestamp of record update

--- contestRemEntry.php
+++ contestRemEntry.php
@@ -3,9 +3,10 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configEnglishContest.php')
 require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/basicLib.php');
 $contestid = $db->real_escape_string(htmlspecialchars($_POST['contestid']));
 
+$nowDate = date("Y-m-d H:i:s", (strtotime("now")));
 $sqlUpdate = <<<SQL
     UPDATE tbl_contest
-    SET status = 1, edited_by = '$login_name'
+    SET status = 1, edited_by = '$login_name', edited_on = '$nowDate'
     WHERE id = $contestid
 SQL;
     if(!$result = $db->query($sqlUpdate)){
