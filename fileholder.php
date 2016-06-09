6d028293c718b7a03d8df72b08d282e7b788a82f
Rick Smoke <rsmoke@umich.edu>
2016-06-09 18:04:16 -0400

    Fix file path in admin docviewer

--- fileholder.php
+++ fileholder.php
@@ -1,5 +1,4 @@
 <?php
   header('Content-Type: application/pdf');
-  //readfile("../contestfiles/" . $_GET['file']);
-  readfile("../contestfiles/" . preg_replace('/[^-a-zA-Z0-9_\.]/', '', $_GET['file']));
+  readfile($_SERVER["DOCUMENT_ROOT"] . '/../contestfiles/' . preg_replace('/[^-a-zA-Z0-9_\.]/', '', $_GET['file']));
 ?>
