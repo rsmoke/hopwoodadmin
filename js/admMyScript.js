2ea8da7755e28a84e7b117a833838b9573186fc5
Rick Smoke <rsmoke@umich.edu>
2016-06-21 13:07:30 -0400

    Add alert for Dev environment if being serverd from csgrsmoke

--- js/admMyScript.js
+++ js/admMyScript.js
@@ -1,4 +1,10 @@
 $( document ).ready(function(){
+
+  var host = window.location.hostname;
+  if (host === "csgrsmoke.lsait.lsa.umich.edu"){
+    $('body').prepend("<div class='bg-danger text-center'>THIS IS A DEVELOPMENT ENVIRONMENT -- THIS IS A DEVELOPMENT ENVIRONMENT -- THIS IS A DEVELOPMENT ENVIRONMENT -- THIS IS A DEVELOPMENT ENVIRONMENT </div>");
+  }
+
   //$('#contest').hide();
   $('#applicant').hide();
   //$('#manuscript_type').hide();
