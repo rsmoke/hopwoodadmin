13a8f0be0a20e1931303f703f8abe6b0313d6b9b
Rick Smoke <rsmoke@umich.edu>
2016-12-01 17:10:52 -0500

    Add check for empty record set being returned

--- js/reportsScript.js
+++ js/reportsScript.js
@@ -9,11 +9,15 @@ $( document ).ready(function(){
           '<thead><th></th><th>UMID</th><th>Last-Name</th><th>First-Name</th><th>Uniqname</th><th>Description</th></thead>'+
           '<tbody>');
     $.getJSON("finAidReport.php", function(data){
+      if ($.isEmptyObject(data.result)){
+        $(".dataout").append("<tr><td colspan='5'>There are no records.</td></td>");
+      } else {
         $.each(data.result, function(){
           $(".dataout").append("<tr><td><button class='btn btn-xs btn-info' data-ID='" + this.uniqname +
            "'><i class='fa fa-info'></i></button></td><td><a href='https://webapps.lsa.umich.edu/UGStuFileV2/App/Cover/Cover.aspx?ID=" + this.umid + "' target='_blank'>" + this.umid + "</a></td><td>" + this.lname + "</td><td>" + this.fname +
             "</td><td>" + this.uniqname + "</td><td>" + this.desc + "</td></tr>");
             });
+      }
     });
         $("#outputReportData").append('</tbody></table>');
 
@@ -27,11 +31,15 @@ $( document ).ready(function(){
           '<thead><th></th><th>Last-Name</th><th>First-Name</th><th>Uniqname</th><th>Recommender1</th><th>Recommender2</th></thead>'+
           '<tbody>');
     $.getJSON("recLetterReport.php", function(data){
+      if ($.isEmptyObject(data.result)){
+        $(".dataout").append("<tr><td colspan='5'>There are no records.</td></td>");
+      } else {
         $.each(data.result, function(){
           $(".dataout").append("<tr><td><button class='btn btn-xs btn-info' data-ID='" + this.entryid +
            "'><i class='fa fa-info'></i></button></td><td>" + this.lname + "</td><td>" + this.fname +
             "</td><td>" + this.uniqname + "</td><td>" + this.recname1 + "</td><td>" + this.recname2 + "</td></tr>");
             });
+      }
     });
         $("#outputReportData").append('</tbody></table>');
 
--- reports.php
+++ reports.php
@@ -97,10 +97,10 @@ $_SESSION['isAdmin'] = true;
           </div>
 <!--          <div class="btn-group" role="group" aria-label="contest_applicants">
             <button id="reportRatingBtn" type="button" class="btn btn-success">Evaluations</button>
-          </div>
-          <div class="btn-group" role="group" aria-label="contest_applicants">
-            <button id="reportNationalEvalBtn" type="button" class="btn btn-success">Hopwood National<br>Evaluations</button>
           </div> -->
+          <div class="btn-group" role="group" aria-label="contest_applicants">
+            <button id="reportNationalEvalBtn" type="button" class="btn btn-success">Hopwood Evaluations</button>
+          </div>
           <div class="btn-group" role="group" aria-label="contest_applicants">
             <a id="ktchensinkBtn" type="button" class="btn btn-info" href="kitchensink.php" data-toggle="tooltip" data-placement="right" title="Click to download details of all the applications ever submitted"><i class="fa fa-download"></i></a>
           </div>
