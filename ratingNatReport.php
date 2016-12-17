27a9fad14d90ddbf45f48d8766903d03419f9621
Rick Smoke <rsmoke@umich.edu>
2016-12-17 18:04:50 -0500

    Add National report logic

---
+++ ratingNatReport.php
@@ -0,0 +1,64 @@
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configEnglishContest.php');
+require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/basicLib.php');
+
+$selectContests = $_GET['id'];
+
+$where = ($selectContests != 10)? "rating > 0 AND " : "";
+
+  $queryRating = <<<SQL
+    SELECT
+    vw.EntryId
+    ,eval.entry_id AS entry_id
+    ,vw.title AS title
+    ,vw.firstname AS firstname
+    ,vw.lastname AS lastname
+    ,vw.umid AS umid
+    ,vw.penName AS penName
+    ,vw.contestName As contestName
+    ,eval.rating AS rating
+    ,eval.evaluator AS evaluator
+    ,vw.document AS document
+    ,eval.contestantcomment AS contestantcomment
+    ,eval.committeecomment AS committeecomment
+    ,vw.manuscriptType AS manuscriptType
+    ,CASE WHEN vw.classLevel > 12 THEN 'G' ELSE 'U' END AS classLevel
+
+    FROM `vw_entrydetail_with_classlevel_currated` AS vw
+    JOIN vw_current_evaluations AS eval ON(vw.EntryID = eval.entry_id)
+    WHERE $where created > '2016-09-01' AND contestsID = $selectContests AND fwdToNational = 1
+    ORDER BY evaluator, rating
+SQL;
+
+
+  $resSelect = $db->query($queryRating);
+  if (!$resSelect) {
+    echo "There is no information available";
+  } else {
+    $result = array();
+
+      while($item = $resSelect->fetch_assoc()){
+      array_push($result, array(
+          'entryid' =>$item["entry_id"],
+          'title' =>$item["title"],
+          'firstname' =>$item["firstname"],
+          'lastname' =>$item["lastname"],
+          'umid' => $item["umid"],
+          'penName' =>$item["penName"],
+          'contestName' => $item["contestName"],
+          'rank' => $item["rating"],
+          'rankedby' => $item["evaluator"],
+          'document' => $item["document"],
+          'contestantcomment' => $item["contestantcomment"],
+          'committeecomment' => $item["committeecomment"],
+          'manuscriptType' => $item["manuscriptType"],
+          'classLevel' => $item["classLevel"]
+          )
+
+        );
+      }
+    }
+
+  echo (json_encode(array("result" => $result)));
+
+  $resSelect->free();
